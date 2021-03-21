cce526787d67b69d1764a3c3f06c0697bad01b9b
vmcguire <victor.mcguire@gmail.com>
2021-03-20 23:48:12 -0700

    add post route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 
+const inputCheck = require("./utils/inputCheck");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -86,6 +88,37 @@ app.delete("/api/candidate/:id", (req, res) => {
 //   }
 //   console.log(result, this.lastID);
 // });
+
+// Create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
